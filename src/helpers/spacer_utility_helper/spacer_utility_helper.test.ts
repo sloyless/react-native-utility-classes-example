44c4c626965024d89dc944e4f8d1bdd857a2c7e7
Sean Loyless <loyless.sean@gmail.com>
2023-04-20 15:07:57 -0400

    Update spacer_utility_helper.test.ts
    Updating tests to match RTL support changes

--- src/helpers/spacer_utility_helper/spacer_utility_helper.test.ts
+++ src/helpers/spacer_utility_helper/spacer_utility_helper.test.ts
@@ -3,13 +3,13 @@ import { applySpacer, applySpacers } from "./spacer_utility_helper";
 describe("SpacerUtilityHelper", () => {
   describe("applySpacers", () => {
     it("translates bootstrap-style spacers correctly", () => {
-      expect(applySpacers(["m-0", "ml-auto", "mx-5"])).toEqual({
+      expect(applySpacers(["m-0", "ms-auto", "mx-5"])).toEqual({
         margin: 0,
         marginLeft: "auto",
         marginHorizontal: expect.any(Number),
       });
 
-      expect(applySpacers(["p-5", "py-3", "pl-5"])).toEqual({
+      expect(applySpacers(["p-5", "py-3", "ps-5"])).toEqual({
         padding: expect.any(Number),
         paddingLeft: expect.any(Number),
         paddingVertical: expect.any(Number),
